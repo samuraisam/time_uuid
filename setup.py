979d93b532f7b91306e3e9a73410b7bf63e7f0c2
Samuel Sutch <samuraiblog@gmail.com>
2012-11-18 18:57:48 -0800

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="time-uuid",
-    version="0.1.0",
+    version="0.1.1",
     url='http://github.com/samuraisam/time_uuid',
     author='Samuel Sutch',
     author_email='samuel.sutch@gmail.com',
